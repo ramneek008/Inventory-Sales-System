3eafc1d01d29a1f9fe3f6c43d15768ee25deae6f
ramneek008 <singhramneek274@gmail.com>
2020-11-03 22:31:47 +0530

    connection established

--- src/com/wipro/sales/util/DBUtil.java
+++ src/com/wipro/sales/util/DBUtil.java
@@ -1,10 +1,21 @@
 package com.wipro.sales.util;
 
+import java.sql.*;
+
 public class DBUtil {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static Connection getDBConnection() {
+		Connection con =null;
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "D@ta00Found");
+		// System.out.println("Connection done");
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		return con;
+		
 	}
 
 }
